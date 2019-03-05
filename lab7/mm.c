7892f3fdfe46a7fa27c8a9f5f81e1199083b4319
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-03-05 18:57:02 +0800

    finish init,free,coalesce,extend_heap function

--- lab7/mm.c
+++ lab7/mm.c
@@ -44,20 +44,91 @@
 #define GET_SIZE(p) (GET(p) & ~0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
-// Given block ptr bp, compute address of next and previous blocks
+// Given block_pointer bp, compute address of next and previous blocks
 #define HDRP(bp) ((char *)(bp)-WSIZE)
 #define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 
-// Given block ptr bp, compute address of next and previous blocks
-#define NEXT_BLKP(bp) ((char *)(bp)+GET_SIZE(((char *)(bp)-WSIZE))
+// Given block_pointer bp, compute address of next and previous blocks
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
 #define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))
 
+// private variables and functions
+static char *heap_listp;               // heap pointer
+static void *extend_heap(size_t size); // extend the heap
+static void *coalesce(void *bp);       // coalesce the free blocks
+
+// extend the heap
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+
+    // Allocate an even number of words to maintain alignment
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+
+    // Initialize free block header/footer and the epilogue header
+    PUT(HDRP(bp), PACK(size, 1));
+    PUT(FTRP(bp), PACK(size, 1));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+    // Coalesce if the previous block was free
+    return coalesce(bp);
+}
+
+// coalesce the free blocks
+static void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc)
+    {
+        return bp;
+    }
+    else if (prev_alloc && !next_alloc)
+    {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+    else if (!prev_alloc && next_alloc)
+    {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    else
+    {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp))) + GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    
+    return bp;
+}
+
 /* 
- * mm_init - initialize the malloc package.cc
- * This function models the virtual memory available to the heap as a large double-word aligned array of bytes.
+ * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
+        return -1;
+
+    PUT(heap_listp, 0);                          //alignment padding
+    PUT(heap_listp + WSIZE, PACK(DSIZE, 1));     //prologue header
+    PUT(heap_listp + 2 * WSIZE, PACK(DSIZE, 1)); //prologue footer
+    PUT(heap_listp + 3 * WSIZE, PACK(0, 1));     //epilogue header
+    heap_listp += (2 * WSIZE);                   //move the heap pointer to the end of the head
+
+    // Extend the empty heap with a free block of CHUNKSIZE bytes
+    if (extend_heap(CHUNKSIZE / WSIZE))
+        return -1;
 
     return 0;
 }
@@ -84,6 +155,11 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    size_t size = GET_SIZE(HDRP(ptr));
+
+    PUT(HDRP(ptr), PACK(size, 0));
+    PUT(FTRP(ptr), PACK(size, 0));
+    coalesce(ptr);
 }
 
 /*
@@ -105,5 +181,3 @@ void *mm_realloc(void *ptr, size_t size)
     mm_free(oldptr);
     return newptr;
 }
-
-
