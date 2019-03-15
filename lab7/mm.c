b285fcc56c70c0e1cb090e58f33ab765af1b32fd
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-03-15 16:02:36 +0800

    Basic finish
    Basic finish

--- lab7/mm.c
+++ lab7/mm.c
@@ -53,36 +53,23 @@
 #define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))
 
 // private variables and functions
-static char *heap_listp;                  // heap pointer
-static void *extend_heap(size_t size);    // extend the heap
-static void *coalesce(void *bp);          // coalesce the free blocks
-static void *find_fit(size_t size);       // search the free list for a fit
-static void place(void *bp, size_t size); // place the block pointer
+static void *heap_listp;                   // heap pointer
+static void *extend_heap(size_t size);     // extend the heap
+static void *coalesce(void *bp);           // coalesce the free blocks
+static void *find_fit(size_t asize);       // search the free list for a fit
+static void place(void *bp, size_t asize); // place the block pointer
 
 // search the free list for a fit
 // first fit
-static void *find_fit(size_t size)
+static void *find_fit(size_t asize)
 {
-    // char *bp = heap_listp;
-    // size_t thissize = GET_SIZE(HDRP(bp));
-    // size_t nextsize = GET_SIZE(HDRP(NEXT_BLKP(bp)));
-
-    // while (thissize >= size && !GET_ALLOC(HDRP(bp)) && nextsize)
-    // {
-    //     bp = NEXT_BLKP(bp);
-    //     thissize = GET_SIZE(HDRP(bp));
-    //     nextsize = GET_SIZE(HDRP(NEXT_BLKP(bp)));
-    // }
-
-    // if (!nextsize)
-    //     return NULL;
-
-    // return bp;
 
     void *bp;
 
-    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0;bp=NEXT_BLKP(bp)){
-        if(!GET_ALLOC(HDRP(bp))&&(size<=GET_SIZE(HDRP(bp)))){
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp))
+    {
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
+        {
             return bp;
         }
     }
@@ -91,23 +78,22 @@ static void *find_fit(size_t size)
 }
 
 // place the block pointer
-static void place(void *bp, size_t size)
+static void place(void *bp, size_t asize)
 {
-    size_t totalsize = GET_SIZE(HDRP(bp));
-    size_t lastsize = totalsize - size;
+    size_t csize = GET_SIZE(HDRP(bp));
 
-    if (lastsize >= (2 * DSIZE))
+    if ((csize - asize) >= (2 * DSIZE))
     {
-        PUT(HDRP(bp), PACK(size, 1));
-        PUT(FTRP(bp), PACK(size, 1));
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
         bp = NEXT_BLKP(bp);
-        PUT(HDRP(bp), PACK(lastsize, 0));
-        PUT(FTRP(bp), PACK(lastsize, 0));
+        PUT(HDRP(bp), PACK(csize - asize, 0));
+        PUT(FTRP(bp), PACK(csize - asize, 0));
     }
     else
     {
-        PUT(HDRP(bp), PACK(size, 1));
-        PUT(FTRP(bp), PACK(size, 1));
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
     }
 }
 
@@ -158,7 +144,7 @@ static void *coalesce(void *bp)
     }
     else // all free
     {
-        size += GET_SIZE(HDRP(NEXT_BLKP(bp))) + GET_SIZE(HDRP(PREV_BLKP(bp)));
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
@@ -182,7 +168,7 @@ int mm_init(void)
     heap_listp += (2 * WSIZE);                   //move the heap pointer to the end of the head
 
     // Extend the empty heap with a free block of CHUNKSIZE bytes
-    if (extend_heap(CHUNKSIZE / WSIZE)==NULL)
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
         return -1;
 
     return 0;
@@ -226,13 +212,13 @@ void *mm_malloc(size_t size)
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *ptr)
+void mm_free(void *bp)
 {
-    size_t size = GET_SIZE(HDRP(ptr));
+    size_t size = GET_SIZE(HDRP(bp));
 
-    PUT(HDRP(ptr), PACK(size, 0));
-    PUT(FTRP(ptr), PACK(size, 0));
-    coalesce(ptr);
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
 }
 
 /*
